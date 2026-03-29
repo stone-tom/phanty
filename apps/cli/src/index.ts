51eaf6ddb8786dfb6f5b2d21f314565fb381898c
Thomas Steiner <thomas.steiner@bitminds.at>
2026-03-29 15:57:50 +0200

    replace some product naming

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -4,7 +4,7 @@ import { pingCommand } from './commands/ping';
 
 const program = new Command();
 
-program.name('cli').description('CLI for eval-lab operations').version('1.0.0');
+program.name('cli').description('CLI for phanty operations').version('1.0.0');
 
 program.addCommand(pingCommand);
 
