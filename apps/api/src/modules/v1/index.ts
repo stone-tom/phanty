714fd2f457268377fba55ac477b81d6a0d6f684f
Thomas Steiner <thomas.steiner@bitminds.at>
2026-04-29 17:03:19 +0200

    add seperate organisation setup endpoint as preparation for custom data seeding e.g. organization block access.

--- apps/api/src/modules/v1/index.ts
+++ apps/api/src/modules/v1/index.ts
@@ -1,5 +1,9 @@
 import Elysia from 'elysia';
+import { organizations } from './organizations';
 import { projects } from './projects';
 import { templates } from './templates';
 
-export const v1 = new Elysia({ prefix: '/v1' }).use(projects).use(templates);
+export const v1 = new Elysia({ prefix: '/v1' })
+  .use(organizations)
+  .use(projects)
+  .use(templates);
